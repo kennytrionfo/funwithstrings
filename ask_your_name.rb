388db605914032cee84a969fef965efe2b481c25
kennytrionfo <kentrionfo@go@gmail.com>
2014-05-22 14:50:34 -0600

    added last name feature

--- ask_your_name.rb
+++ ask_your_name.rb
@@ -3,3 +3,9 @@ puts "What is your first name?"
 name = gets
 
 puts "Nice to meet you #{name}"
+
+puts "What's your last name?"
+
+last_name = gets
+
+puts "Did you know that your last name reversed is #{last_name.reverse}"
