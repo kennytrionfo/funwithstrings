22dc96fec78a324dacc31b59b4a27efbbcafadf0
kennytrionfo <kentrionfo@go@gmail.com>
2014-05-22 15:12:26 -0600

    added fun last name note about the length of their last name

--- ask_your_name.rb
+++ ask_your_name.rb
@@ -9,3 +9,4 @@ puts "What's your last name?"
 last_name = gets.chomp
 
 puts "Did you know that your last name reversed is #{last_name.reverse}"
+puts "And by the way #{firstname}, did you also know that your last name has #{last_name.count} letters in it?"
